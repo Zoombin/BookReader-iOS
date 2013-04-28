41d2f93bacea2950428e05414b5f344d159a169e
yc <ycabc1989@163.com>
2013-04-28 15:39:12 +0800

    修改送礼物界面

--- BookReader/UI/GiftViewController.h
+++ BookReader/UI/GiftViewController.h
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 #import "Book.h"
+#import "GiftCell.h";
 
-@interface GiftViewController : UIViewController <UITableViewDataSource,UITableViewDelegate>
+@interface GiftViewController : UIViewController <UITableViewDataSource,UITableViewDelegate,GiftCellDelegate>
 - (id)initWithIndex:(NSString *)index andBookObj:(Book *)bookObject;
 @end
