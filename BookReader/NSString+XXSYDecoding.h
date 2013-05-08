65ece4c7731e387761c78cb74c36ab4396a8f88d
Bin <30135878@qq.com>
2013-05-08 17:34:22 +0800

    Migrate MD5 method to XXSYDecoding category

--- BookReader/NSString+XXSYDecoding.h
+++ BookReader/NSString+XXSYDecoding.h
@@ -7,6 +7,37 @@
 //
 
 #import <Foundation/Foundation.h>
+#import<CommonCrypto/CommonDigest.h>
+
+
+@interface NSString (md5)
+- (NSString *) md516;
+- (NSString *) md532;
+@end
+
+
+@implementation NSString (md5)
+- (NSString *) md516
+{
+    NSString *md5String = [self md532];
+    NSMutableString *returnString = [NSMutableString string];
+    for (int i = 8; i<24; i++) {
+        [returnString appendFormat:@"%c",[md5String characterAtIndex:i]];
+    }
+    return [returnString lowercaseString];
+}
+
+- (NSString *) md532
+{
+    const char *cStr = [self UTF8String];
+    unsigned char result[16];
+    CC_MD5( cStr, strlen(cStr),result );
+    NSMutableString *hash =[NSMutableString string];
+    for (int i = 0; i < 16; i++)
+        [hash appendFormat:@"%02X", result[i]];
+    return [hash lowercaseString];
+}
+@end
 
 @interface NSString (XXSYDecoding)
 
