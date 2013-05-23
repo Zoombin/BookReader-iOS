35afcfc5089e3a63d21e1745d549c7ad0dd6ebc4
yc <ycabc1989@163.com>
2013-05-23 12:16:27 +0800

    缩小时间的Label大小

--- BookReader/UI/CommentCell.m
+++ BookReader/UI/CommentCell.m
@@ -12,6 +12,8 @@
 
 @implementation CommentCell {
     UILabel *messageLabel;
+    UILabel *timeLabel;
+    UIView *background;
 }
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
@@ -19,11 +21,21 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         // Initialization code
-        messageLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, self.contentView.frame.size.width, self.contentView.frame.size.height)];
+        background = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.contentView.frame.size.width,self.contentView.frame.size.height)];
+        [background setBackgroundColor:[UIColor colorWithRed:230.0/255.0 green:227.0/255.0 blue:220.0/255.0 alpha:1.0]];
+        [self.contentView addSubview:background];
+        
+        messageLabel = [[UILabel alloc]initWithFrame:CGRectMake(5, 1, self.contentView.frame.size.width-20, self.contentView.frame.size.height)];
         [messageLabel setFont:[UIFont systemFontOfSize:14]];
+        [messageLabel setBackgroundColor:[UIColor clearColor]];
         [messageLabel setNumberOfLines:0];
         [messageLabel setLineBreakMode:NSLineBreakByCharWrapping];
         [self.contentView addSubview:messageLabel];
+        
+        timeLabel = [[UILabel alloc] init];
+        [timeLabel setBackgroundColor:[UIColor clearColor]];
+        [timeLabel setFont:[UIFont systemFontOfSize:10]];
+        [self.contentView addSubview:timeLabel];
     }
     return self;
 }
@@ -32,9 +44,13 @@
 {
     CGRect cellFrame = [self frame];
     cellFrame.origin = CGPointMake(0, 0);
-    [messageLabel setText:[[NSString stringWithFormat:@"%@:%@\n%@",comment.userName,comment.content,comment.insertTime] XXSYHandleRedundantTags]];
+    [messageLabel setText:[[NSString stringWithFormat:@"%@:%@",comment.userName,comment.content] XXSYHandleRedundantTags]];
     [messageLabel sizeToFit];
-    cellFrame.size.height =  messageLabel.frame.size.height;
+    [timeLabel setText:comment.insertTime];
+    [timeLabel setFrame:CGRectMake(5, 1+messageLabel.frame.size.height, messageLabel.frame.size.width, 20)];
+    cellFrame.size.height =  messageLabel.frame.size.height+timeLabel.frame.size.height+10;
+    background.frame = CGRectMake(cellFrame.origin.x, cellFrame.origin.y
+                                  , cellFrame.size.width, cellFrame.size.height-10);
     [self setFrame:cellFrame];
 }
 
