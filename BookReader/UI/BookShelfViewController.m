d8a4e31560fb7a4fc16d079c810f373ef17019d1
Bin <30135878@qq.com>
2013-07-07 19:57:26 +0800

    更新推荐书籍

--- BookReader/UI/BookShelfViewController.m
+++ BookReader/UI/BookShelfViewController.m
@@ -139,7 +139,7 @@ static NSString *kStartSyncAutoSubscribeNotification = @"start_sync_auto_subscri
 					[booksForDisplay removeAllObjects];
 					[booksForDisplay addObjectsFromArray:books];
 					[booksView reloadData];
-					//[[NSNotificationCenter defaultCenter] postNotificationName:kStartSyncChaptersNotification object:nil];
+					[[NSNotificationCenter defaultCenter] postNotificationName:kStartSyncChaptersNotification object:nil];
 				}];
 			}];
         }
@@ -149,7 +149,6 @@ static NSString *kStartSyncAutoSubscribeNotification = @"start_sync_auto_subscri
 - (void)syncBooks
 {
 	if (stopAllSync) return;
-	if (![ServiceManager userID]) return;
 	
 //	[self displayHUD:@"同步收藏..."];
 	syncing = YES;
