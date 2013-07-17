e267219d8d10acd265c90ca153754830b89fa2f3
yc <ycabc1989@163.com>
2013-07-17 10:47:28 +0800

    修改推荐的title的背景图片

--- BookReader/UI/BookStoreViewController.m
+++ BookReader/UI/BookStoreViewController.m
@@ -500,9 +500,9 @@
         return nil;
     }
     UIImageView *view = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, 30)];
-    [view setBackgroundColor:[UIColor colorWithRed:189.0/255.0 green:173.0/255.0 blue:136.0/255.0 alpha:0.7]];
-//    [view.layer addSublayer:[self shadowAsInverse]];
+    [view setImage:[UIImage imageNamed:@"bookstore_recommend_title"]];
     UILabel *label = [UILabel bookStoreLabelWithFrame:CGRectMake(0, 0, view.bounds.size.width, 30)];
+    [label setTextColor:[UIColor blackColor]];
     for (int i = 0; i<[recommendTitlesArray count]; i++) {
         if (section == i) {
             [label setText:[@"  " stringByAppendingString:[recommendTitlesArray objectAtIndex:i]]];
@@ -510,17 +510,6 @@
     }
     [view addSubview:label];
     return view;
-}
-
-- (CAGradientLayer *)shadowAsInverse
-{
-    CAGradientLayer *newShadow = [[CAGradientLayer alloc] init];
-    
-    CGRect newShadowFrame = CGRectMake(0, 0, 320, 30);
-    newShadow.frame = newShadowFrame;
-    //添加渐变的颜色组合
-    newShadow.colors = [NSArray arrayWithObjects:(id)[UIColor colorWithRed:189.0/255.0 green:173.0/255.0 blue:136.0/255.0 alpha:0.7].CGColor,(id)[UIColor whiteColor].CGColor,nil];
-    return newShadow;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
