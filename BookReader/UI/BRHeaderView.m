8496cfd0efdbc081a20dc64adc9042890aa9d9a7
yc <ycabc1989@163.com>
2013-08-09 18:33:17 +0800

    优化BRHeaderView

--- BookReader/UI/BRHeaderView.m
+++ BookReader/UI/BRHeaderView.m
@@ -51,7 +51,6 @@
 - (void)addButtons {
     CGRect BOOKSTORE_BUTTON_FRAME = CGRectMake(self.bounds.size.width-10-50,3,50,32);
     CGRect MYACCOUNT_BUTTON_FRAME = CGRectMake(BOOKSTORE_BUTTON_FRAME.origin.x-50,3,50,32);
-    
     CGRect EDIT_BUTTON_FRAME = CGRectMake(10, 3, 50, 32);
     CGRect UPDATE_BUTTON_FRAME = CGRectMake(EDIT_BUTTON_FRAME.origin.x+50, 3, 50, 32);
     CGRect FINISH_BUTTON_FRAME = EDIT_BUTTON_FRAME;
@@ -67,45 +66,36 @@
     [_backButton setHidden:YES];
     [self bringSubviewToFront:_titleLabel];
     
-    NSArray *titles = @[@"书城", @"", @"编辑", @"",@"完成",@"删除"];//会员 更新
-    NSArray *rectStrings = @[NSStringFromCGRect(BOOKSTORE_BUTTON_FRAME), NSStringFromCGRect(MYACCOUNT_BUTTON_FRAME),NSStringFromCGRect(EDIT_BUTTON_FRAME),NSStringFromCGRect(UPDATE_BUTTON_FRAME),NSStringFromCGRect(FINISH_BUTTON_FRAME),NSStringFromCGRect(DELETE_BUTTON_FRAME)];
-    NSArray *selectorStrings = @[@"bButtonClick", @"mButtonClick",@"eButtonClick",@"uButtonClick",@"fButtonClick",@"dButtonClick"];
-    
-    #define UIIMAGE(x) [UIImage imageNamed:x]
-    NSArray *styles = @[@(BookReaderButtonStyleRight),@(BookReaderButtonStyleLeft),@(BookReaderButtonStyleLeft),@(BookReaderButtonStyleRight),@(BookReaderButtonStyleNormal),@(BookReaderButtonStyleNormal)];
-    
-    UIButton *bookStoreBtn = [UIButton addButtonWithFrame:CGRectFromString(rectStrings[0]) andStyle:[styles[0] intValue]];
-    [bookStoreBtn setTitle:titles[0] forState:UIControlStateNormal];
-    [bookStoreBtn addTarget:self action:NSSelectorFromString(selectorStrings[0]) forControlEvents:UIControlEventTouchUpInside];
+    UIButton *bookStoreBtn = [UIButton addButtonWithFrame:BOOKSTORE_BUTTON_FRAME andStyle:BookReaderButtonStyleRight];
+    [bookStoreBtn setTitle:@"书城" forState:UIControlStateNormal];
+    [bookStoreBtn addTarget:self action:@selector(bButtonClick) forControlEvents:UIControlEventTouchUpInside];
     [headerViewOne addSubview:bookStoreBtn];
     
-    UIButton *memberBtn = [UIButton addButtonWithFrame:CGRectFromString(rectStrings[1]) andStyle:[styles[1] intValue]];
+    UIButton *memberBtn = [UIButton addButtonWithFrame:MYACCOUNT_BUTTON_FRAME andStyle:BookReaderButtonStyleLeft];
     [memberBtn setImage:[UIImage imageNamed:@"shelf_member_btn"] forState:UIControlStateNormal];
-    [memberBtn setTitle:titles[1] forState:UIControlStateNormal];
-    [memberBtn addTarget:self action:NSSelectorFromString(selectorStrings[1]) forControlEvents:UIControlEventTouchUpInside];
+    [memberBtn addTarget:self action:@selector(mButtonClick) forControlEvents:UIControlEventTouchUpInside];
     [headerViewOne addSubview:memberBtn];
     
-    UIButton *editBtn = [UIButton addButtonWithFrame:CGRectFromString(rectStrings[2]) andStyle:[styles[2] intValue]];
-    [editBtn setTitle:titles[2] forState:UIControlStateNormal];
-    [editBtn addTarget:self action:NSSelectorFromString(selectorStrings[2]) forControlEvents:UIControlEventTouchUpInside];
+    UIButton *editBtn = [UIButton addButtonWithFrame:EDIT_BUTTON_FRAME andStyle:BookReaderButtonStyleLeft];
+    [editBtn setTitle:@"编辑" forState:UIControlStateNormal];
+    [editBtn addTarget:self action:@selector(eButtonClick) forControlEvents:UIControlEventTouchUpInside];
     [headerViewOne addSubview:editBtn];
     
-    refreshButton = [UIButton addButtonWithFrame:CGRectFromString(rectStrings[3]) andStyle:[styles[3] intValue]];
+    refreshButton = [UIButton addButtonWithFrame:UPDATE_BUTTON_FRAME andStyle:BookReaderButtonStyleRight];
     [refreshButton setImage:[UIImage imageNamed:@"refresh_btn"] forState:UIControlStateNormal];
-    [refreshButton setTitle:titles[3] forState:UIControlStateNormal];
-    [refreshButton addTarget:self action:NSSelectorFromString(selectorStrings[3]) forControlEvents:UIControlEventTouchUpInside];
+    [refreshButton addTarget:self action:@selector(uButtonClick) forControlEvents:UIControlEventTouchUpInside];
     [headerViewOne addSubview:refreshButton];
     
-    UIButton *finishBtn = [UIButton addButtonWithFrame:CGRectFromString(rectStrings[4]) andStyle:[styles[4] intValue]];
-    [finishBtn setTitle:titles[4] forState:UIControlStateNormal];
-    [finishBtn addTarget:self action:NSSelectorFromString(selectorStrings[4]) forControlEvents:UIControlEventTouchUpInside];
+    UIButton *finishBtn = [UIButton addButtonWithFrame:FINISH_BUTTON_FRAME andStyle:BookReaderButtonStyleNormal];
+    [finishBtn setTitle:@"完成" forState:UIControlStateNormal];
+    [finishBtn addTarget:self action:@selector(fButtonClick) forControlEvents:UIControlEventTouchUpInside];
     [headerViewTwo addSubview:finishBtn];
     
-    deleteButton = [UIButton addButtonWithFrame:CGRectFromString(rectStrings[5]) andStyle:[styles[5] intValue]];
+    deleteButton = [UIButton addButtonWithFrame:DELETE_BUTTON_FRAME andStyle:BookReaderButtonStyleNormal];
     [deleteButton setUserInteractionEnabled:NO];
     [deleteButton setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
-    [deleteButton setTitle:titles[5] forState:UIControlStateNormal];
-    [deleteButton addTarget:self action:NSSelectorFromString(selectorStrings[5]) forControlEvents:UIControlEventTouchUpInside];
+    [deleteButton setTitle:@"删除" forState:UIControlStateNormal];
+    [deleteButton addTarget:self action:@selector(dButtonClick) forControlEvents:UIControlEventTouchUpInside];
     [headerViewTwo addSubview:deleteButton];
 }
 
